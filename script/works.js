119d24b2c59d68b132e45c10036696793a9fdb19
KimGeon139 <163296243+KimGeon139@users.noreply.github.com>
2024-06-19 18:05:21 +0900

    main-end

--- script/works.js
+++ script/works.js
@@ -1,5 +1,5 @@
 const carouselSlide = document.querySelector('.a');
-const carouselImages = document.querySelectorAll('.a   img');
+const carouselImages = document.querySelectorAll('.a  .bcdimg');
 
 const prevBtn = document.querySelector('#prevBtn');
 const nextBtn = document.querySelector('#nextBtn');
@@ -22,22 +22,3 @@ prevBtn.addEventListener('click', () => {
   counter--;
   carouselSlide.style.transform = 'translateX(' + -size * counter + 'px)';
 });
-
-// // Jump to First/Last Slide
-// carouselSlide.addEventListener('transitionend', () => {
-//   console.log(carouselImages[counter]);
-//   if (carouselImages[counter].id === 'lastClone') {
-//     carouselSlide.style.transition = 'none'; // 트랜지션 효과 없애기
-
-//     counter = carouselImages.length - 2; // couter 초기화
-
-//     carouselSlide.style.transform =
-//       'translateX(' + -size * counter + 'px)'; // 실제 마지막 이미지로 이동.
-//   } else if (carouselImages[counter].id === 'firstClone') {
-//     carouselSlide.style.transition = 'none';
-
-//     counter = carouselImages.length - counter; // couter 초기화
-//     carouselSlide.style.transform =
-//       'translateX(' + -size * counter + 'px)';
-//   }
-// });
